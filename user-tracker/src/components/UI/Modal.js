c7f6a5a7a4c98d6fa156f787fae1c3701eee390c
nagy4 <robert.nagy@knapp.com>
2022-05-17 18:00:39 +0200

    Login project. useEffect. Portals. Refs.

--- user-tracker/src/components/UI/Modal.js
+++ user-tracker/src/components/UI/Modal.js
@@ -1,23 +1,45 @@
+import React from 'react'
 import Button from './Button'
 import Card from './Card'
 import styles from './Modal.module.css'
+import ReactDOM from 'react-dom'
+
+function Backdrop(props) {
+    return <div className={styles.backdrop} onClick={props.onOkay}></div>
+}
+
+function ModalOverlay(props) {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={props.onOkay}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
 
 function Modal(props) {
     return (
-        <div>
-            <div className={styles.backdrop} onClick={props.onOkay}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={props.onOkay}>Okay</Button>
-                </footer>
-            </Card>
-        </div>
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onOkay={props.onOkay} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onOkay={props.onOkay}
+                />,
+                document.getElementById('overlay-root')
+            )}
+        </React.Fragment>
     )
 }
 
--- user-tracker/src/components/Users/AddUser.js
+++ user-tracker/src/components/Users/AddUser.js
@@ -1,53 +1,75 @@
-
-import Card from "../UI/Card"
-import Button from "../UI/Button"
+import Card from '../UI/Card'
+import Button from '../UI/Button'
 import styles from './AddUser.module.css'
-import { useState } from "react";
-import Modal from "../UI/Modal";
+import { useState, useRef } from 'react'
+import Modal from '../UI/Modal'
+import Wrapper from '../Helpers/Wrapper'
 
 function AddUser(props) {
-
-    const [userName, setUserName] = useState('')
-    const [age, setAge] = useState('')
-    const [error, setError] = useState({error: false, title: '', message: ''})
+    const nameInputRef = useRef()
+    const ageInputRef = useRef()
+    const [error, setError] = useState({ error: false, title: '', message: '' })
     const addUserHandler = (event) => {
+        const enteredName = nameInputRef.current.value
+        const enteredAge = ageInputRef.current.value
         event.preventDefault()
-        if (userName.trim().length === 0 || age.trim().length === 0) {
-            setError({error:true, title: "Invalid input", message: "Please give a name and age!"})
-            return 
-        }
-        if (parseInt(age) < 1) {
-            setError({error:true, title: "Invalid age", message: "Age must be greater than 0!"})
+        if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                error: true,
+                title: 'Invalid input',
+                message: 'Please give a name and age!',
+            })
             return
         }
-        props.onAddUser({name: userName, age: age, key: Math.random().toString()})
-        setUserName('')
-        setAge('')
-
-            
+        if (parseInt(enteredAge) < 1) {
+            setError({
+                error: true,
+                title: 'Invalid age',
+                message: 'Age must be greater than 0!',
+            })
+            return
+        }
+        props.onAddUser({
+            name: enteredName,
+            age: enteredAge,
+            key: Math.random().toString(),
+        })
+        nameInputRef.current.value = ''
+        ageInputRef.current.value = ''
     }
 
-    const userNameChangeHandler = (event) => {
-        setUserName(event.target.value)
-    }
-
-    const ageChangeHandler = (event) => {
-        setAge(event.target.value)
-    }
-
-    const onOkayHandler = event => setError({error:false, title: '', message: ''})
+    const onOkayHandler = (event) =>
+        setError({ error: false, title: '', message: '' })
 
     return (
-        <div>
-        {error.error && <Modal title={error.title} message={error.message} onOkay={onOkayHandler}/>}
-        <Card className={styles.input}><form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" onChange={userNameChangeHandler} value={userName}/>
-            <label htmlFor="age">Age</label>
-            <input type="number" name="age" id="age" onChange={ageChangeHandler} value={age}/>
-            <Button type="submit">Add User</Button>
-        </form></Card>
-        </div>
+        <Wrapper>
+            {error.error && (
+                <Modal
+                    title={error.title}
+                    message={error.message}
+                    onOkay={onOkayHandler}
+                />
+            )}
+            <Card className={styles.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor='username'>Username</label>
+                    <input
+                        type='text'
+                        name='username'
+                        id='username'
+                        ref={nameInputRef}
+                    />
+                    <label htmlFor='age'>Age</label>
+                    <input
+                        type='number'
+                        name='age'
+                        id='age'
+                        ref={ageInputRef}
+                    />
+                    <Button type='submit'>Add User</Button>
+                </form>
+            </Card>
+        </Wrapper>
     )
 }
 
